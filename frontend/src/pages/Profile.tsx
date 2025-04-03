ff88c1687ec32ae9b60d5fbcbb1e8159fa9c9fcb
MrunalKankarej <164769913+MrunalKankarej@users.noreply.github.com>
2025-04-03 23:58:02 +0400

    Update Profile.tsx
    Minor updates in profile display

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -32,6 +32,7 @@ const Profile: React.FC = () => {
     >
       {/* Profile Avatar at the Top Left */}
       <Avatar
+      src="https://randomuser.me/api/portraits/men/44.jpg"
         sx={{
           width: 200,
           height: 200,
@@ -39,7 +40,7 @@ const Profile: React.FC = () => {
           top: 40,
           left: 40,
         }}
-        src="" // Empty src for default avatar
+         // Empty src for default avatar
       />
 
       {/* User Info */}
@@ -68,6 +69,16 @@ const Profile: React.FC = () => {
           <Grid item xs={4}>
             <Typography variant="h6">
               <strong>Organization:</strong> ABC Hospital
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+
+      <CardContent sx={{ marginLeft: "10px" }}>
+      <Grid container spacing={10} marginTop={2} >
+          <Grid item xs={4}>
+            <Typography variant="h6">
+              <strong>About Me:</strong> Hi! I am Dr. David Wilson, a radiologist specializing in MRI and CT imaging. I have always admired the power of medical imaging to uncover what happens beneath the layers and help patients and doctors get the answers they need for effective diagnosis. Over my 5 years at ABC Hospital, I have learned that every scan tells a story, and my job is to interpret it with precision and care. I am also passionate about using the latest technology to detect and diagnose conditions early, giving patients and their doctors the best path forward. At the heart of my work is a commitment to accuracy, clarity, and, most importantly, patient well-being.
             </Typography>
           </Grid>
         </Grid>
